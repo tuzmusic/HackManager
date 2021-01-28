5f21ab6f595decd75c9c8734d3f294e18f040206
Jonathan Tuzman <tuzmusic@gmail.com>
2021-01-28 14:46:14 -0500

    Initial proof-of-concept mock of FS within the test file (more actual changes written)

--- src/Assembler.ts
+++ src/Assembler.ts
@@ -3,9 +3,10 @@ import * as fs from 'fs';
 
 export const Assembler: AssemblerInterface = class {
   static assemble(filepath: string): void {
-  
+    
     const content = fs.readFileSync(filepath).toString();
-  
+    
     fs.writeFileSync(filepath.replace('asm', 'hack'), content);
   }
 };
+
