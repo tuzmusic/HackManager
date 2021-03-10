cab0625204ea0e5558ef9478696fd804c1136bb4
Jonathan Tuzman <tuzmusic@gmail.com>
2021-03-10 13:09:57 -0500

    write initial script runner

--- src/VMTranslator/src/vm-translator-app.ts
+++ src/VMTranslator/src/vm-translator-app.ts
@@ -1,4 +1,22 @@
+import childProcess from 'child_process';
 import { VMTranslator } from './VMTranslator';
 
 const pathString = process.argv[2];
 VMTranslator.processPath(pathString);
+
+function testFile(filepath: string) {
+  const cpuEmPath = '~/dev/HackManager/tools/CPUEmulator.sh';
+  const fixturesPath = '~/dev/HackManager/src/VMTranslator/fixtures/';
+  
+  console.log('---');
+  console.log(filepath.split('/').pop() + ': ');
+  try {
+    childProcess.execSync(cpuEmPath + ' ' + fixturesPath + filepath).toString();
+    console.log('Passed!');
+  } catch ({ message }) {
+    // error is automatically printed to console
+  }
+  console.log('---');
+}
+
+testFile('FunctionCalls/FibonacciElement/FibonacciElement.tst');
